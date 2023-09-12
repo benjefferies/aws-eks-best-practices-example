fc3770d096700cdc04737bf18fa756af8bcea58d
Ben Jefferies <benjefferies@echosoft.uk>
2023-09-12 12:22:55 +0100

    chore(eks-addons): remove unnecessary addons from eks-addons module
    The coredns, vpc-cni, and kube-proxy addons were removed from the eks-addons module as they are not needed for the current configuration. This change simplifies the module and reduces unnecessary resources.

--- infrastructure/modules/eks-addons/main.tf
+++ infrastructure/modules/eks-addons/main.tf
@@ -85,15 +85,6 @@ module "eks_blueprints_addons" {
     aws-ebs-csi-driver = {
       most_recent = true
     }
-    coredns = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
   }
 
   aws_for_fluentbit = {
